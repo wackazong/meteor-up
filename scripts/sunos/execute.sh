fff865a6cdeb30e66bd801b5cd7452092fb95586
wackazong <alexander@wackazong.com>
2015-02-25 00:58:12 +0100

    use env.sh for environment variables

--- scripts/sunos/execute.sh
+++ scripts/sunos/execute.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# provide environment variables to custom script
-APP_NAME="<%= appName %>"
-ROOT_URL="<%= rootUrl %>"
-MONGO_URL="<%= mongoUrl %>"
-NODE_ENV="<%= nodeEnv %>"
-PORT="<%= port %>"
-HTTP_FORWARDED_COUNT="<%= httpForwardedCount %>"
-BIND_IP="<%= bindIp %>"
-UPSTART_UID="<%= upstartUid %>"
+# set environment variables that can be used by the custom script
+source /opt/<%= appName %>/config/env.sh
 
 <%- script %>
