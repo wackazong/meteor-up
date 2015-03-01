adef1850014aa586a9e0e885af0d534dd73017fa
wackazong <alexander@wackazong.com>
2015-03-01 01:33:59 +0100

    add APP_NAME to env.sh

--- templates/linux/env.sh
+++ templates/linux/env.sh
@@ -2,6 +2,7 @@
 export PORT=80
 export MONGO_URL=mongodb://127.0.0.1/<%= appName %>
 export ROOT_URL=http://localhost
+export APP_NAME=<%= appName %>
 
 #it is possible to override above env-vars from the user-provided values
 <% for(var key in env) { %>
